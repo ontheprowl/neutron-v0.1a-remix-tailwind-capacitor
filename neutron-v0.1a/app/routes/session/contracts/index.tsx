8ffe99640d467f7a6e04d0e4e73868ed22a540f4
ontheprowl <indiaontheprowl@gmail.com>
2022-07-07 21:54:19 +0530

    Last Commit before personalized urls

--- neutron-v0.1a/app/routes/session/contracts/index.tsx
+++ neutron-v0.1a/app/routes/session/contracts/index.tsx
@@ -1,10 +1,9 @@
 import { useNavigate, useLoaderData, Outlet, Link, useSubmit } from '@remix-run/react'
 import { ActionFunction, LoaderFunction, redirect } from '@remix-run/server-runtime';
-import * as React from 'react'
 import { json } from 'remix-utils';
-import { firestore, auth } from '../../../firebase/neutron-config';
+import { firestore, auth } from '../../../firebase/neutron-config.server';
 import { useCollection, useCollectionData } from 'react-firebase-hooks/firestore'
-import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, DocumentData, getDocs } from 'firebase/firestore';
 import { formatDateToReadableString } from '~/utils/utils';
 import { primaryGradientDark, secondaryGradient } from '~/utils/neutron-theme-extensions';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -12,16 +11,20 @@ import ViewIcon from '~/components/inputs/ViewIcon';
 import EditIcon from '~/components/inputs/EditIcon';
 import DeleteIcon from '~/components/inputs/DeleteIcon';
 import ChatIcon from '~/components/inputs/ChatIcon';
+import { useState } from 'react';
+import { dataflow } from 'googleapis/build/src/apis/dataflow';
+import MobileNavbarPadding from '~/components/layout/MobileNavbarPadding';
+import { getFirebaseDocs } from '~/firebase/queries.server';
+import { requireUser } from '~/session.server';
+import { UserState } from '~/models/user';
 
 export const loader: LoaderFunction = async ({ request }) => {
+    const session = await requireUser(request, true);
 
-    const querySnapshot = await getDocs(collection(firestore, "contracts"));
-    const result: any[] = []
-    querySnapshot.forEach((doc) => {
-        result.push({ id: doc.id, data: doc.data() });
-    });
+    
 
-    return json(result);
+    const result = await getFirebaseDocs('contracts')
+    return json({ contracts: result, metadata: session?.metadata });
 
 }
 
@@ -40,12 +43,114 @@ export const action: ActionFunction = async ({ request }) => {
 export default function ListContracts() {
 
 
-    const [user, loading, error] = useAuthState(auth);
-    const contracts = useLoaderData();
+    const loaderData = JSON.parse(useLoaderData());
+    const disputes = [];
+    const contracts = loaderData.contracts;
+    const currentUser = loaderData.metadata;
+
+    const [contractsTab, setContractsTab] = useState(true);
+    const [currentContract, setCurrentContract] = useState(-1);
 
     const submit = useSubmit();
 
     let navigate = useNavigate();
+
+
+
+    const generateContractsList = () => {
+        return (<ul className="m-5 mt-1 space-y-4 max-h-96 overflow-scroll snap-y snap-mandatory">
+            {contracts.map((contract: { id: string, data: any }, index: number) => {
+                return (<li onClick={() => {
+                    setCurrentContract(index)
+                }} key={contract.id} className={`snap-center bg-bg-secondary-dark border-2 h-auto rounded-xl border-accent-dark dark:bg-gray-800 dark:border-gray-700 hover:bg-gradient-to-br from-violet-700 via-violet-400 to-violet-500 hover:bg-opacity-50 dark:hover:bg-gray-600 flex flex-col transition-all justify-between`}>
+
+                    <div className="flex flex-row justify-between">
+                        <div className="flex flex-col  m-2 p-3 space-y-2">
+                            <h2 className="prose prose-md text-white">
+                                {contract.data.projectName}
+                            </h2>
+                            <h3 className="prose prose-sm text-gray-300">{contract.data.clientName}</h3>
+                            <h4 className="prose prose-sm text-gray-300">{contract.data.signedDate}</h4>
+                        </div>
+                        <div className="flex flex-col  m-2 p-3 justify-between">
+                            <h2 className="prose prose-md text-white">
+                                {contract.data.totalValue}
+                            </h2>
+                            <td>
+                                <h3 className="font-medium text-black bg-gray-100 text-center rounded-lg p-1">Draft</h3>
+                            </td>
+                        </div>
+                    </div>
+
+
+                    <div className={` m-2 p-3 justify-between ${currentContract == index ? 'flex flex-row' : 'hidden'}`}>
+                        <ViewIcon onClick={() => {
+                            navigate(`${contract.id}`)
+                        }} className={''}></ViewIcon>
+                        <EditIcon onClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+                            throw new Error('Function not implemented.');
+                        }} className={''}></EditIcon>
+                        <DeleteIcon onClick={(e) => {
+                            let data = new FormData();
+                            data.append('id', contract.id);
+                            submit(data,
+                                { method: 'post' });
+                        }} className={''}></DeleteIcon>
+                        <ChatIcon onClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+                            throw new Error('Function not implemented.');
+                        }} className={''}></ChatIcon>
+                    </div>
+
+
+                </li>)
+            })
+            }
+        </ul >)
+    }
+
+    const generateDisputesList = () => {
+        return (<ul className="m-5 mt-1 space-y-4 max-h-60 overflow-scroll">
+            {disputes.map((contract: { id?: string, data?: any }, index: number) => {
+                return (<li onClick={() => {
+                    navigate(`${contract.id}`);
+                }} key={contract.id} className={`bg-bg-secondary-dark border-2 h-auto rounded-xl border-accent-dark dark:bg-gray-800 dark:border-gray-700 hover:bg-gradient-to-br from-violet-700 via-violet-400 to-violet-500 hover:bg-opacity-50 dark:hover:bg-gray-600 flex flex-row justify-between`}>
+
+                    <div className="flex flex-col  m-2 p-3 space-y-2">
+                        <h2 className="prose prose-md text-white">
+                            {contract.data.projectName}
+                        </h2>
+                        <h3 className="prose prose-sm text-gray-300">{contract.data.clientName}</h3>
+                        <h4 className="prose prose-sm text-gray-300">{contract.data.signedDate}</h4>
+                    </div>
+                    <div className="flex flex-col  m-2 p-3 justify-between">
+                        <h2 className="prose prose-md text-white">
+                            {contract.data.totalValue}
+                        </h2>
+                        <td>
+                            <h3 className="font-medium text-black bg-gray-100 text-center rounded-lg p-1">Draft</h3>
+                        </td>
+                    </div>
+
+
+                    {/* <td><ViewIcon onClick={() => {
+                    navigate(`${contract.id}`)
+                }} className={''}></ViewIcon></td>
+                <td><EditIcon onClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+                    throw new Error('Function not implemented.');
+                }} className={''}></EditIcon></td>
+                <td><DeleteIcon onClick={(e) => {
+                    let data = new FormData();
+                    data.append('id', contract.id);
+                    submit(data,
+                        { method: 'post' });
+                }} className={''}></DeleteIcon></td>
+                <td><ChatIcon onClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+                    throw new Error('Function not implemented.');
+                }} className={''}></ChatIcon></td> */}
+                </li>)
+            })}
+        </ul>)
+    }
 
     return (
         <div className='flex flex-col space-y-8 bg-bg-primary-dark h-full'>
@@ -53,17 +158,17 @@ export default function ListContracts() {
                 <div className='flex flex-col m-6 justify-between'>
                     <div className="flex flex-col">
                         <article className="prose">
-                            <h2 className="text-white prose prose-lg">Welcome {user?.email}</h2>
+                            <h2 className="text-white prose prose-lg">Welcome {currentUser?.email}</h2>
                             <p className="text-white prose prose-sm">{formatDateToReadableString()}</p>
                         </article>
                     </div>
-                    <div className="flex items-center w-[692px] mt-10">
+                    <div className="flex items-center w-full sm:w-[692px] mt-10">
                         <label htmlFor="simple-search" className="sr-only">Search</label>
                         <div className="relative w-full ">
                             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg className="w-5 h-5 text-white dark:text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                             </div>
-                            <input type="text" id="simple-search" className="p-5 bg-bg-primary-dark border border-gray-300 text-gray-900 text-sm rounded-lg placeholder-white block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white " placeholder="Search through contracts" required />
+                            <input type="text" id="simple-search" className="p-5 bg-bg-primary-dark border border-gray-300 text-gray-900 text-sm rounded-lg placeholder-white block w-full pl-10  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white " placeholder="Search through contracts" required />
 
                         </div>
                     </div>
@@ -74,7 +179,7 @@ export default function ListContracts() {
                     </div>
                 </div>
 
-                <div className="flex flex-col-reverse m-6">
+                <div className="hidden sm:flex sm:flex-col-reverse m-6">
 
                     {/**Add profile buttons here */}
                     <button
@@ -87,8 +192,27 @@ export default function ListContracts() {
                     </button>
                 </div>
             </div>
-            <div className={`bg-bg-primary-dark p-3 rounded-lg border-2 border-solid border-accent-dark  h-full m-6`}>
-                <table className=" w-full h-auto text-sm text-left text-gray-500 dark:text-gray-400">
+            <div className="flex flex-row sm:hidden m-3 w-auto rounded-xl">
+                <button
+                    className={`w-full rounded-lg ${contractsTab ? 'bg-accent-dark' : 'bg-bg-primary-dark'}  p-3 text-white transition-all hover:scale-100`}
+                    onClick={() => {
+                        setContractsTab(!contractsTab)
+
+                    }}
+                >
+                    Contracts
+                </button>
+                <button
+                    className={`w-full rounded-lg ${!contractsTab ? 'bg-accent-dark' : 'bg-bg-primary-dark'} p-3 text-white transition-all hover:scale-100`}
+                    onClick={() => {
+                        setContractsTab(!contractsTab)
+                    }}
+                >
+                    Disputes
+                </button>
+            </div>
+            <div className={`bg-bg-primary-dark p-3 rounded-lg border-2 border-solid border-accent-dark  h-auto m-6 sm:flex hidden`}>
+                <table className="w-full max-h-max text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs w-auto text-white uppercase bg-bg-primary-dark dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" className="px-6 py-3 text-center">
@@ -112,7 +236,7 @@ export default function ListContracts() {
                         </tr>
                     </thead>
                     <tbody>
-                        {JSON.parse(contracts).map((contract: { id: string, data: any }, index: number) => {
+                        {contracts.map((contract: { id: string, data: any }, index: number) => {
                             return (<tr key={contract.id} className={`bg-bg-primary-dark border-t border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gradient-to-br from-violet-700 via-violet-400 to-violet-500 hover:bg-opacity-50 dark:hover:bg-gray-600`}>
 
                                 <th scope="row" className="px-6 py-4 font-medium text-center text-white dark:text-white whitespace-nowrap">
@@ -155,5 +279,12 @@ export default function ListContracts() {
                     </tbody>
                 </table>
             </div>
+            <div className="flex flex-col sm:hidden">
+                {contractsTab ? generateContractsList() : generateDisputesList()}
+            </div>
+            <MobileNavbarPadding />
+
         </div >);
 }
+
+
